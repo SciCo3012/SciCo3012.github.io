072be0c206be863b08abef4a80bbe2e6ae03cd81
SciCo3012 <126389361+SciCo3012@users.noreply.github.com>
2023-03-01 18:15:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 /*JavaScript*/
-const names = ['Adhiraj', 'Ananya', 'Aditya', 'Arjun', 'Ditya', 'Anant'];
+const names = ['Adhiraj and Ananya', 'Aditya and Ditya', 'Anant and Ridhwan', 'Arjun and Amol'];
 
 const name1 = document.querySelector('.name-container:nth-child(1) h1');
-const name2 = document.querySelector('.name-container:nth-child(2) h1');
 
 function getRandomName() {
     const randomNumber = Math.floor(Math.random() * names.length);
@@ -10,4 +9,3 @@ function getRandomName() {
 }
 
 name1.textContent = getRandomName();
-name2.textContent = getRandomName();
